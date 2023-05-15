12852581618f4d532000812b8572264562cdaf13
yhuzued <yusufmait@gmail.com>
2023-05-15 14:26:22 +0700

    Resolve path entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './controller/main.js',
+  entry: './src/controller/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
