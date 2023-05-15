a8e5789ed43d52d75e265feeb1e6c4714431649f
yhuzued <yusufmait@gmail.com>
2023-05-15 14:18:42 +0700

    Refactor add task to database and view logic

--- src/main.js
+++ src/main.js
@@ -5,29 +5,36 @@ import { addProject as domAddProject, addTask as domAddTask } from './domManipul
 const form = document.querySelector('form');
 const todos = todoDatabase();
 
-function isProjectExist(todo) {
-  return todos.database.some((task) => task.project.name === todo);
+function createNewProject(task, project) {
+  const newProject = new TodoFactory(task, project);
+  todos.add(newProject);
+  domAddProject(project);
+  domAddTask(task);
+  newProject.addTodo(task);
+}
+
+function insertToProject(task, project) {
+  const existingProject = todos.database.find((obj) => obj.project.name === project);
+  existingProject.addTodo(task);
+  domAddTask(task);
+  return existingProject;
+}
+
+function isProjectExist(project) {
+  return todos.database.some((task) => task.project.name === project);
 }
 
 function addTask(task, project = 'default') {
   if (!isProjectExist(project)) {
-    const newProject = new TodoFactory(task, project);
-    todos.add(newProject);
-    domAddProject(project);
-    domAddTask(task);
-    newProject.addTodo(task);
-    return newProject;
+    createNewProject(task, project);
+  } else {
+    insertToProject(task, project);
   }
-
-  const todosExist = todos.database.find((obj) => obj.project.name === project);
-  todosExist.addTodo(task);
-  domAddTask(task);
-  return todosExist;
 }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const taskform = document.querySelector('#task');
-  addTask(taskform.value);
+  const taskInput = document.querySelector('#task');
+  addTask(taskInput.value);
   form.reset();
 });
