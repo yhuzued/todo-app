80613cbf5972064af14976b7c87609ced74ec814
yhuzued <yusufmait@gmail.com>
2023-05-18 11:23:05 +0700

    Refactor logic to create project and insert task

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -1,17 +1,14 @@
 import { Project, Task } from '../model/project';
 import { todos } from '../model/todoDatabase';
-import { addElementDom } from '../view/domManipulation';
 import { saveTodoList } from './databaseController';
-
-function createNewProject(task, project) {
-  const newProject = new Project(project);
-  newProject.tasks.push(new Task(task));
-  todos.database.push(newProject);
-}
 
 function insertToProject(task, project) {
   const existProject = todos.database.find((obj) => obj.name === project);
   existProject.tasks.push(new Task(task));
+}
+function createNewProject(project) {
+  const newProject = new Project(project);
+  todos.database.push(newProject);
 }
 
 function isProjectExist(project) {
@@ -19,12 +16,12 @@ function isProjectExist(project) {
 }
 
 export default function addTask(task, project = 'Default Folder') {
-  if (isProjectExist(project)) {
-    insertToProject(task, project);
-  } else {
-    createNewProject(task, project);
-    addElementDom('.project-list', project);
+  if (!isProjectExist(project)) {
+    createNewProject(project);
   }
-  addElementDom('.tasks-list', task);
+
+  insertToProject(task, project);
   saveTodoList();
 }
+
+export { addTask, createNewProject };
