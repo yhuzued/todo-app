f8d73ddae932779a6d2498449a7b6d1ee86f120d
yhuzued <yusufmait@gmail.com>
2023-05-18 10:07:03 +0700

    Create project object

--- src/model/project.js
+++ src/model/project.js
@@ -1,4 +1,18 @@
-export default function Project(name) {
-  this.name = name;
-  this.tasks = [];
+import { startOfToday, format } from 'date-fns';
+
+class Project {
+  constructor(name) {
+    this.name = name;
+    this.tasks = [];
+  }
 }
+
+function Task(title) {
+  return {
+    title,
+    isCompleted: false,
+    creteadAt: format(startOfToday(), 'dd MM yyyy'),
+  };
+}
+
+export { Project, Task };
