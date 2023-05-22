09ed1311c6b9f3f880e246aecbeef91ed89cb526
yhuzued <yusufmait@gmail.com>
2023-05-22 13:57:36 +0700

    Add more icon

--- src/controller/domController.js
+++ src/controller/domController.js
@@ -1,6 +1,8 @@
 import { formatDistanceToNow, parse } from 'date-fns';
 import { todos } from '../model/todoDatabase';
 import circleOutline from '../assets/icon/circle-outline.svg';
+import deleteIcon from '../assets/icon/delete.svg';
+import folderIcon from '../assets/icon/folder-open-outline.svg';
 
 const hookElement = (element) => document.querySelector(element);
 
@@ -23,20 +25,30 @@ const listTask = (task) => {
   createdAt.textContent = `Created ${distance} ago`;
   createdAt.classList.add('text-xs');
 
-  const deleteButton = createElement('p');
-  deleteButton.textContent = 'X';
+  const deleteButton = new Image();
+  deleteButton.src = deleteIcon;
+  deleteButton.classList.add('delete', 'h-6', 'w-6', 'cursor-pointer', 'hover:scale-105');
 
   mainContainer.append(checkbox, title, createdAt, deleteButton);
   return mainContainer;
 };
 
 const createProjectElement = (project) => {
-  const projectElement = document.createElement('li');
-  const paragraphElement = document.createElement('p');
+  const projectElement = createElement('li');
+  const container = createElement('div');
+  container.classList.add('item-project', 'flex', 'gap-2', 'w-fit', 'hover:outline', 'w-full', 'rounded-md', 'p-2', 'cursor-pointer');
 
+  const folder = new Image();
+  folder.src = folderIcon;
+  folder.classList.add('folder', 'h-6', 'w-6', 'cursor-pointer', 'hover:scale-105');
+
+  const paragraphElement = createElement('p');
   paragraphElement.textContent = project;
   paragraphElement.classList.add('cursor-pointer', 'w-fit', 'item-project');
-  projectElement.append(paragraphElement);
+
+  container.append(folder, paragraphElement);
+
+  projectElement.append(container);
 
   return projectElement;
 };
