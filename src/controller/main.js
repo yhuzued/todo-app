8bc74216e43ba097659485e761c167ddd98df1e3
yhuzued <yusufmait@gmail.com>
2023-05-22 09:36:51 +0700

    Refactor main.js

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,7 +1,36 @@
-import '../view/style.css';
-import { refreshDom } from './domController';
-import { getTodoList, isTodoListExist } from './databaseController';
+import '../assets/style.css';
+import refreshDom from './domController';
+import { getTodoList, isTodoListExist, saveTodoList } from './databaseController';
 import { addTask, createNewProject } from './todoController';
+
+const handleProjectItemClick = (e) => {
+  if (e.target.classList.contains('item-project')) {
+    document.getElementById('title').textContent = e.target.textContent;
+    refreshDom();
+  }
+};
+
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+
+  if (e.target.id === 'new-task-form') {
+    const taskInput = document.querySelector('#task');
+    const project = document.getElementById('title');
+    addTask(taskInput.value, project.textContent);
+  }
+
+  if (e.target.id === 'new-project-form') {
+    const projectInput = document.querySelector('#project');
+    createNewProject(projectInput.value);
+    saveTodoList();
+  }
+
+  refreshDom();
+  e.target.reset();
+};
+
+document.addEventListener('click', handleProjectItemClick);
+document.addEventListener('submit', handleFormSubmit);
 
 if (isTodoListExist()) {
   getTodoList();
@@ -10,26 +39,3 @@ if (isTodoListExist()) {
 }
 
 refreshDom();
-
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('item-project')) {
-    document.getElementById('title').textContent = e.target.textContent;
-    refreshDom();
-  }
-});
-
-document.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (e.target.id === 'new-task-form') {
-    const taskInput = document.querySelector('#task');
-    const project = document.getElementById('title');
-    addTask(taskInput.value, project.textContent);
-  }
-
-  if (e.target.id === 'new-project-form') {
-    const taskInput = document.querySelector('#project');
-    createNewProject(taskInput.value);
-  }
-  refreshDom();
-  e.target.reset();
-});
