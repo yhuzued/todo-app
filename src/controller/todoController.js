d70856b3751c61d30e6f1f9681a2a04cdf624e7c
yhuzued <yusufmait@gmail.com>
2023-05-23 13:35:02 +0700

    Add ability to delete project and task

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -1,14 +1,45 @@
 import { Project, Task } from '../model/project';
 import { todos } from '../model/todoDatabase';
-import { saveTodoList } from './databaseController';
 
 function insertToProject(task, project) {
   const existProject = todos.database.find((obj) => obj.name === project);
+
+  if (existProject.tasks.some((tsk) => tsk.title === task)) {
+    return;
+  }
+
+  if (task === '') {
+    return;
+  }
+
   existProject.tasks.push(new Task(task));
 }
+
 function createNewProject(project) {
+  if (project === '') {
+    return;
+  }
+
   const newProject = new Project(project);
   todos.database.push(newProject);
+}
+
+function deleteProject(projectName) {
+  const project = todos.database.find((prj) => prj.name === projectName);
+  const projectIndex = todos.database.indexOf(project);
+  todos.database.splice(projectIndex, 1);
+}
+
+function deleteTask(projectName, taskName) {
+  const project = todos.database.find((prj) => prj.name === projectName);
+  const task = project.tasks.find((tsk) => tsk.title === taskName);
+  const taskIndex = project.tasks.indexOf(task);
+
+  todos.database.forEach((prj) => {
+    if (prj.name === projectName) {
+      prj.tasks.splice(taskIndex, 1);
+    }
+  });
 }
 
 function isProjectExist(project) {
@@ -21,7 +52,8 @@ export default function addTask(task, project = 'My Day') {
   }
 
   insertToProject(task, project);
-  saveTodoList();
 }
 
-export { addTask, createNewProject, isProjectExist };
+export {
+  addTask, createNewProject, isProjectExist, deleteProject, deleteTask,
+};
